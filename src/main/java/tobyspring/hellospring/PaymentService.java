beeece30e4109e31e775238bfabbece82ef30b90
doydoit <doydoit@gmail.com>
2024-07-17 23:40:44 +0900

    섹션 3 : 오브젝트와 의존관계
    - 인터페이스의 도입
    - 관계설정 책임의 분리

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -6,16 +6,16 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class PaymentService {
-    private final WebApiExRateProvider exRateProvider;
+    private final ExRateProvider exRateProvider;
 
-    public PaymentService() {
-        this.exRateProvider = new WebApiExRateProvider();
+    public PaymentService(ExRateProvider exRateProvider) {
+        this.exRateProvider = exRateProvider;
     }
 
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
 
         // ~~ 환율 가져오기 ~~ -> 코드를 이해하는데 문제가 없다면 주석은 제거한다.
-        BigDecimal exRate = exRateProvider.getWebExRate(currency);
+        BigDecimal exRate = exRateProvider.getExRate(currency);
 
         // ~~ 금액 계산 ~~ -> 코드를 이해하는데 문제가 없다면 주석은 제거한다.
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
