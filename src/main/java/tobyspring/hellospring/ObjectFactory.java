686956e0b0bfd8cccc9c8bc686b48acb49183d93
doydoit <doydoit@gmail.com>
2024-07-22 08:59:54 +0900

    섹션 3 : 오브젝트와 의존관계
    - 구성정보를 가져오는 다른 방법

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,11 +1,13 @@
 package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class ObjectFactory {
-    @Bean // 각각 하나의 빈을 만드는 메서들이기 때문에 @Bean을 붙여준다.
+    /*@Bean // 각각 하나의 빈을 만드는 메서들이기 때문에 @Bean을 붙여준다.
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
@@ -13,5 +15,5 @@ public class ObjectFactory {
     @Bean // 각각 하나의 빈을 만드는 메서들이기 때문에 @Bean을 붙여준다.
     public ExRateProvider exRateProvider() {
         return new SimpleExRateProvider();
-    }
+    }*/
 }
