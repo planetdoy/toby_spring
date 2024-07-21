5a09299e0dcdf38d66217d6c6b0a6548a52eba45
doydoit <doydoit@gmail.com>
2024-07-22 08:50:22 +0900

    섹션 3 : 오브젝트와 의존관계
    - 스프링 컨테이너와 의존관계 주입

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,11 +1,17 @@
 package tobyspring.hellospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
+    @Bean // 각각 하나의 빈을 만드는 메서들이기 때문에 @Bean을 붙여준다.
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
 
+    @Bean // 각각 하나의 빈을 만드는 메서들이기 때문에 @Bean을 붙여준다.
     public ExRateProvider exRateProvider() {
-        return new WebApiExRateProvider();
+        return new SimpleExRateProvider();
     }
 }
