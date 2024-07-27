5f824f0d8fcd8f47925f7480e7c2f62238255de8
doydoit <doydoit@gmail.com>
2024-07-27 16:26:41 +0900

    섹션 4 : 테스트
    - 자동으로 수행되는 테스트

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -15,21 +15,28 @@ public class Client {
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         Payment payment1 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment1);
+        System.out.println("Payment: " + payment1);
 
-        System.out.println("--------------------------------------\n");
 
-        TimeUnit.SECONDS.sleep(1);
-
-        Payment payment2 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment2);
-
-        System.out.println("--------------------------------------\n");
-
-        TimeUnit.SECONDS.sleep(2);
-
-        Payment payment3 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment3);
+        /**
+         * 데코레이터 패턴 예시
+         * */
+//        Payment payment1 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+//        System.out.println(payment1);
+//
+//        System.out.println("--------------------------------------\n");
+//
+//        TimeUnit.SECONDS.sleep(1);
+//
+//        Payment payment2 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+//        System.out.println(payment2);
+//
+//        System.out.println("--------------------------------------\n");
+//
+//        TimeUnit.SECONDS.sleep(2);
+//
+//        Payment payment3 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+//        System.out.println(payment3);
 
 
 
--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -12,13 +12,13 @@ import tobyspring.hellospring.payment.PaymentService;
 public class ObjectFactory {
     @Bean // 각각 하나의 빈을 만드는 메서들이기 때문에 @Bean을 붙여준다.
     public PaymentService paymentService() {
-        return new PaymentService(cachedExRateProvider());
+        return new PaymentService(exRateProvider());
     }
 
-    @Bean // 각각 하나의 빈을 만드는 메서들이기 때문에 @Bean을 붙여준다.
-    public ExRateProvider cachedExRateProvider() {
-        return new CachedExRateProvider(exRateProvider());
-    }
+//    @Bean // 각각 하나의 빈을 만드는 메서들이기 때문에 @Bean을 붙여준다.
+//    public ExRateProvider cachedExRateProvider() {
+//        return new CachedExRateProvider(exRateProvider());
+//    }
 
     @Bean // 각각 하나의 빈을 만드는 메서들이기 때문에 @Bean을 붙여준다.
     public ExRateProvider exRateProvider() {
