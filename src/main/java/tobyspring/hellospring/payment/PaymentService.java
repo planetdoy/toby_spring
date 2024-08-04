22c36f29bb9b4e50264f53ee939048d03f1746e2
doydoit <doydoit@gmail.com>
2024-08-04 15:56:47 +0900

    섹션 4 : 테스트
    - 도메인 오브젝트 테스트

--- src/main/java/tobyspring/hellospring/payment/PaymentService.java
+++ src/main/java/tobyspring/hellospring/payment/PaymentService.java
@@ -18,17 +18,9 @@ public class PaymentService {
     }
 
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-
-        // ~~ 환율 가져오기 ~~ -> 코드를 이해하는데 문제가 없다면 주석은 제거한다.
         BigDecimal exRate = exRateProvider.getExRate(currency);
 
-        // ~~ 금액 계산 ~~ -> 코드를 이해하는데 문제가 없다면 주석은 제거한다.
-        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-
-        // ~~ 유효 시간 계산 ~~ -> 코드를 이해하는데 문제가 없다면 주석은 제거한다.
-        LocalDateTime validUtil = LocalDateTime.now(clock).plusMinutes(30);
-
-        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUtil);
+        return Payment.createPrepared(orderId, currency, foreignCurrencyAmount, exRate, LocalDateTime.now(clock));
     }
 
 }
