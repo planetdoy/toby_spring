c4920a926f23fcfce721c5c136138a6562f99a37
doydoit <doydoit@gmail.com>
2024-08-25 19:08:51 +0900

    섹션 6 : 예외
    - Order 리포지토리와 예외

--- src/main/java/tobyspring/hellospring/DataClient.java
+++ src/main/java/tobyspring/hellospring/DataClient.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import tobyspring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.order.Order;
 
 import java.math.BigDecimal;
@@ -11,20 +12,14 @@ import java.math.BigDecimal;
 public class DataClient {
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
-        EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
-
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
+        OrderRepository repository = beanFactory.getBean(OrderRepository.class);
 
         Order order = new Order("100", BigDecimal.TEN);
+        repository.save(order);
 
         System.out.println(order);
 
-        em.persist(order);
-
-        System.out.println(order);
-
-        em.getTransaction().commit();
-        em.close();
+        Order order2 = new Order("100", BigDecimal.ONE);
+        repository.save(order2);
     }
 }
