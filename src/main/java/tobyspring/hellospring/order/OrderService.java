686c2c6eb627b46b2f1015e7073b79e71be20bbd
doydoit <doydoit@gmail.com>
2024-09-01 23:17:12 +0900

    섹션 7 : 서비스 추상화
    - 트랜잭션 서비스 추상화

--- src/main/java/tobyspring/hellospring/order/OrderService.java
+++ src/main/java/tobyspring/hellospring/order/OrderService.java
@@ -2,6 +2,7 @@ package tobyspring.hellospring.order;
 
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 import tobyspring.hellospring.data.JpaOrderRepository;
 
@@ -10,9 +11,9 @@ import java.math.BigDecimal;
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final JpaTransactionManager transactionManager;
+    private final PlatformTransactionManager transactionManager;
 
-    public OrderService (OrderRepository orderRepository, JpaTransactionManager transactionManager) {
+    public OrderService (OrderRepository orderRepository, PlatformTransactionManager transactionManager) {
         this.orderRepository = orderRepository;
         this.transactionManager = transactionManager;
     }
