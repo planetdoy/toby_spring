6be93f81e8a600597534cc142e248d6946330041
doydoit <doydoit@gmail.com>
2024-09-02 07:51:35 +0900

    섹션 7 : 서비스 추상화
    - JDBC 데이터 액세스 기술

--- src/main/java/tobyspring/hellospring/OrderConfig.java
+++ src/main/java/tobyspring/hellospring/OrderConfig.java
@@ -4,20 +4,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import tobyspring.hellospring.data.JdbcOrderRepository;
 import tobyspring.hellospring.data.JpaOrderRepository;
 import tobyspring.hellospring.order.OrderRepository;
 import tobyspring.hellospring.order.OrderService;
+
+import javax.sql.DataSource;
 
 @Configuration
 @Import(DataConfig.class) // 이 설정으로 OrderConfig 값을 가져올 때 DataConfig 값도 모두 가져온다.
 public class OrderConfig {
     @Bean
-    public OrderRepository orderRepository() {
-        return new JpaOrderRepository();
+    public OrderRepository orderRepository(DataSource dataSource) {
+        return new JdbcOrderRepository(dataSource);
     }
 
     @Bean
-    public OrderService orderService(JpaTransactionManager transactionManager) {
-        return new OrderService(orderRepository(), transactionManager);
+    public OrderService orderService(PlatformTransactionManager transactionManager,
+                                     OrderRepository orderRepository) {
+        return new OrderService(orderRepository, transactionManager);
     }
 }
--- src/main/java/tobyspring/hellospring/order/Order.java
+++ src/main/java/tobyspring/hellospring/order/Order.java
@@ -23,6 +23,10 @@ public class Order {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getNo() {
         return no;
     }
